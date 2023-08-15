55c675b8ce8e8e963eac35ac9d6fc2f82151133a
EnverJ <Enlem1987@gmail.com>
2023-08-14 21:45:19 -0600

    function

---
+++ function.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function Hello() {
+echo "hello"
+}
+
+#or
+ quit() {
+exit
+}
+#quit
+ Hello
+#quit
+echo "foo"
+quit
---
+++ functionPrint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function print() {
+echo $1 $2 $3
+}
+
+#or
+ quit() {
+exit
+}
+#quit
+print  Hello world !
+#quit
+echo "foo"
+quit
